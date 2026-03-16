fe9047603a3d392811753b302c4d18d89a4e7c35
Satya <sa7ya.30@gmail.com>
2026-03-16 18:22:23 +0530

    add SSL connection

---
+++ .vite/deps/@radix-ui_react-slot.js
@@ -0,0 +1,16 @@
+import {
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+} from "./chunk-FGWY4ZA4.js";
+import "./chunk-PSFTFTKK.js";
+import "./chunk-C6XPCYDW.js";
+import "./chunk-G3PMV62Z.js";
+export {
+  Slot as Root,
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+};
--- server/db.ts
+++ server/db.ts
@@ -11,5 +11,15 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const isProduction = process.env.NODE_ENV === "production";
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false,
+});
+
 export const db = drizzle(pool, { schema });
